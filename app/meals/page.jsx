759e8e73cb926ff5b25ffc8dc17d29a271bdb9b7
Raje <rajeshwar.work09@gmail.com>
2025-03-07 22:45:56 +0530

    refactor: loading component for meals page and implement Suspense for async data fetching

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,12 +1,17 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 
 import MealsGrid from "../../components/meals/meals-grid";
 import { getMeals } from "../../lib/meals";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={styles.header}>
@@ -22,7 +27,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={styles.loading}>Fetching Meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
