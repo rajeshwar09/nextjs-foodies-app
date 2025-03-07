838642ff9136011daef1e522a5fc1437f2967c46
Raje <rajeshwar.work09@gmail.com>
2025-03-07 22:39:10 +0530

    feat: Add loading component for meals page

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+import styles from "./loading.module.css"
+
+const MealsLoadingPage = () => {
+  return (
+    <p className={styles.loading}>Fetching Meals...</p>
+  )
+}
+
+export default MealsLoadingPage
