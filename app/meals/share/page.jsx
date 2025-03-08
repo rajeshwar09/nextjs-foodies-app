c851d6181f51e3f69e4464aa8de85fb8a386f86b
Raje <rajeshwar.work09@gmail.com>
2025-03-08 12:00:15 +0530

    feat: Added meal sharing with form validation and error handling

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,9 +1,12 @@
+"use client"
+import { useFormState } from "react-dom";
 import ImagePicker from "../../../components/meals/image-picker";
 import MealsFormSubmit from "../../../components/meals/meals-form-submit";
 import { shareMeal } from "../../../lib/action";
 import classes from "./page.module.css";
 
 const ShareMealPage = () => {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +16,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +45,7 @@ const ShareMealPage = () => {
             ></textarea>
           </p>
           <ImagePicker label="Select Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
